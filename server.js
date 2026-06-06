c37d0944b366583ddf2c186bee6f331ac7c55ee5
Rahul R P <147433645+rprahul07@users.noreply.github.com>
2026-06-06 20:39:27 +0530

    Refactor server.js to use PORT constant

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const PORT=3000;
 
 const userRoutes = require('./routes/userRoutes');  // this
 
@@ -9,6 +10,6 @@ app.use(express.json());// this
 
 app.use('/profile', userRoutes);// this
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
